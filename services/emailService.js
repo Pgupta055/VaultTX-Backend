c7903524bdd103ba823bb6d27689fbffcc93546d
Priyanshu Gupta <priyanshugupta1110@gmail.com>
2026-07-29 17:09:12 +0530

    Try Gmail SMTP over port 465

--- services/emailService.js
+++ services/emailService.js
@@ -3,12 +3,15 @@ console.log("EMAIL_USER:", process.env.EMAIL_USER);
 console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 const sendEmail = async ({ to, subject, html }) => {
